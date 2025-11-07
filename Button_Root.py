e09e79a23b70137cf7c13ee5235310425416a2f3
Axl-M <axlMalyasov@gmail.com>
2025-11-08 06:20:06 +1000

    keywords self, root, app

---
+++ Button_Root.py
@@ -0,0 +1,17 @@
+# Демонстрация использования ключевого слова root
+
+from kivy.app import App
+from kivy.lang import Builder
+
+KV = '''
+BoxLayout:
+    orientation: 'vertical'
+    Button
+        text: root.orientation
+'''
+
+class MainApp(App):
+    def build(self):
+        return Builder.load_string(KV)
+
+MainApp().run()
